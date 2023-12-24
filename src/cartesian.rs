d3c0d80718e76c9bacdf5834559277769975b188
dsmtE <desmet.enguerrand@gmail.com>
2023-12-24 01:30:12 +0100

    ✨ add Display for Direction

--- src/cartesian.rs
+++ src/cartesian.rs
@@ -290,3 +290,14 @@ impl Direction {
     }
 }
 
+impl std::fmt::Display for Direction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", match self {
+            Direction::Up => '^',
+            Direction::Down => 'v',
+            Direction::Left => '<',
+            Direction::Right => '>',
+        })
+    }
+}
+
