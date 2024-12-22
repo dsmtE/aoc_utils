06ece5b1514d376007468e172033ada48713ae3e
dsmtE <desmet.enguerrand@gmail.com>
2024-12-22 19:46:05 +0100

    ✨ Fix number parsing to support negative number

--- src/nom_parsing.rs
+++ src/nom_parsing.rs
@@ -1,12 +1,9 @@
 use nom::{
-    IResult,
-    character::complete::{digit1, space1},
-    combinator::map_res,
-    multi::separated_list1,
+    bytes::complete::tag, character::complete::{digit1, space1}, combinator::{map_res, opt, recognize}, multi::separated_list1, sequence::preceded, IResult
 };
 
 pub fn number<T: std::str::FromStr>(input: &str) -> IResult<&str, T> {
-    map_res(digit1, str::parse::<T>)(input)
+    map_res(recognize(preceded(opt(tag("-")), digit1)), str::parse::<T>)(input)
 }
 
 pub fn numbers_list<T: std::str::FromStr>(input: &str) -> IResult<&str, Vec<T>> {
